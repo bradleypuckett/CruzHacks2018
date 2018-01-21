c42842c4fadae37b07468fc55f81e2ab4f7a086b
mocklee <mock7ee@gmail.com>
2018-01-20 16:50:39 -0800

    Added Tokbox dependencies

--- Application/app/src/main/java/lango/link/com/langolink/Main.java
+++ Application/app/src/main/java/lango/link/com/langolink/Main.java
@@ -2,12 +2,14 @@ package lango.link.com.langolink;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
