d717be80062acc7568a6e1da288d7481726cf498
mocklee <mock7ee@gmail.com>
2018-01-21 02:00:11 -0800

    Added server-side login functionality

--- Application/app/src/main/java/lango/link/com/langolink/Create.java
+++ Application/app/src/main/java/lango/link/com/langolink/Create.java
@@ -9,14 +9,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Create extends AppCompatActivity {
 
-    private EditText user_Name;
+    private EditText username;
     private EditText password;
     private EditText first_Name;
     private EditText last_Name;
     private EditText email;
+    private EditText inputip;
+    private EditText inputPort;
     private Button create_account;
+    private String portString;
+    private String userString;
+    private String passString;
+    private String firstString;
+    private String lastString;
+    private String emailString;
+    private String ipString;
+
+    Socket kkSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +42,68 @@ public class Create extends AppCompatActivity {
 
 
         // Initialize all the buttons and boxes
+        init();
 
-        user_Name = (EditText)findViewById(R.id.username_edit_text);
-        password = (EditText)findViewById(R.id.password_edit_text);
-
-        first_Name = (EditText)findViewById(R.id.first_name_edit_text);
-        last_Name = (EditText)findViewById(R.id.last_name_edit_text);
-        email = (EditText)findViewById(R.id.email_edit_text);
-        create_account = (Button)findViewById(R.id.create_account_button);
 
         // Executes if user clicks create account
         create_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Go to create
-                launchMain();
+                userString = username.getText().toString();
+                passString= password.getText().toString();
+                firstString = first_Name.getText().toString();
+                lastString = last_Name.getText().toString();
+                emailString = email.getText().toString();
+                ipString = inputip.getText().toString();
+                portString = inputPort.getText().toString();
+
+                GlobalVars.ip = ipString;
+                GlobalVars.port = Integer.parseInt(portString);
+
+                try {
+                    kkSocket = new Socket(GlobalVars.ip, GlobalVars.port);
+                    GlobalVars.out = new PrintWriter(kkSocket.getOutputStream(), true);
+                    GlobalVars.in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+                }catch(Exception e){
+
+                }
+                // Validate account creation details
+                if(( userString.length() != 0) && ( passString.length() != 0) && ( firstString.length() != 0) && ( lastString.length() != 0) &&( emailString.length() != 0) &&( ipString.length() != 0) &&( portString.length() != 0))
+                {
+
+                    try {
+                        if(NetworkIO.createProfile( GlobalVars.out, GlobalVars.in, userString, passString, emailString, firstString, lastString))
+                        {
+                            // Valid User
+                            launchMain();
+                        }
+                        else
+                        {
+                            Toast.makeText(getBaseContext(), "Account creation failed.", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(getBaseContext(), "Please fill all fields." , Toast.LENGTH_LONG).show();
+                }
             }
         });
 
+    }
+
+    private void init() {
+        username = (EditText)findViewById(R.id.username_edit_text);
+        password = (EditText)findViewById(R.id.password_edit_text);
+        first_Name = (EditText)findViewById(R.id.first_name_edit_text);
+        last_Name = (EditText)findViewById(R.id.last_name_edit_text);
+        email = (EditText)findViewById(R.id.email_edit_text);
+        create_account = (Button)findViewById(R.id.create_account_button);
+        inputip = (EditText)findViewById(R.id.ip_edit_text);
+        inputPort = (EditText)findViewById(R.id.port_edit_text);
     }
 
     private void launchMain() {
--- Application/app/src/main/java/lango/link/com/langolink/Login.java
+++ Application/app/src/main/java/lango/link/com/langolink/Login.java
@@ -20,10 +20,13 @@ public class Login extends AppCompatActivity {
 
     private EditText user_Name;
     private EditText password;
+    private EditText inputip;
+    private EditText inputPort;
     private Button login;
     private Button create_account;
     private String username;
     private String pass;
+    private String portstring;
 
 
     Socket kkSocket;
@@ -34,14 +37,6 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         init();
 
-        try {
-            kkSocket = new Socket(hostName, portNumber);
-            out = new PrintWriter(kkSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
-        }catch(Exception e){
-
-        }
-
         // Executes if user clicks login
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,20 +45,36 @@ public class Login extends AppCompatActivity {
                 username = user_Name.getText().toString();
                 pass = password.getText().toString();
 
-                if(( username.length() != 0) && ( pass.length() != 0) ){
-                    // Validate accounts
+                GlobalVars.ip = inputip.getText().toString();
+                portstring = inputPort.getText().toString();
+                GlobalVars.port = Integer.parseInt(portstring);
+
+                try {
+                    kkSocket = new Socket(GlobalVars.ip, GlobalVars.port);
+                    GlobalVars.out = new PrintWriter(kkSocket.getOutputStream(), true);
+                    GlobalVars.in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+                }catch(Exception e){
+
+                }
+                if(( username.length() != 0) && ( pass.length() != 0) )
+                {
+                    // Validate account
                     try {
-                        if(NetworkIO.sendLogin( GlobalVars.out, GlobalVars.in, username, pass)
+                        if(NetworkIO.sendLogin( GlobalVars.out, GlobalVars.in, username, pass))
+                        {
+                            // Valid User
+                            launchMain();
+                        }
+                        else
+                        {
+                            Toast.makeText(getBaseContext(), "Username or password invalid.", Toast.LENGTH_LONG).show();
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
-                    } {
-                        launchMain();
-                    }
-                    else{
-                        Toast.makeText(getBaseContext(), "Username or password invalid.", Toast.LENGTH_LONG).show();
                     }
                 }
-                else{
+                else
+                {
                     Toast.makeText(getBaseContext(), "Please enter a valid username and password." , Toast.LENGTH_LONG).show();
                 }
             }
@@ -85,13 +96,8 @@ public class Login extends AppCompatActivity {
         password = (EditText)findViewById(R.id.password_edit_text);
         login = (Button)findViewById(R.id.login_button);
         create_account = (Button)findViewById(R.id.create_account_button);
-
-        GlobalVars.ip = "127.0.0.1";
-        GlobalVars.port = 55455;
-        GlobalVars.out = null;
-        GlobalVars.in = null;
-
-
+        inputip = (EditText)findViewById(R.id.ip_edit_text);
+        inputPort = (EditText)findViewById(R.id.port_edit_text);
     }
 
     private void launchMain() {
