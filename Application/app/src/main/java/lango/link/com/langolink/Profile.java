b6c79410269b6f66bb75adb5a6768eeb12409b46
mocklee <mock7ee@gmail.com>
2018-01-21 11:44:35 -0800

    Fixed camera and audio permissions

--- Application/app/src/main/java/lango/link/com/langolink/Profile.java
+++ Application/app/src/main/java/lango/link/com/langolink/Profile.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+import android.app.Activity;
+
+
 
 public class Profile extends AppCompatActivity {
     private Button call;
@@ -12,7 +17,8 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECORD_AUDIO}, 1);
         call = (Button)findViewById(R.id.call_button);
 
         call.setOnClickListener(new View.OnClickListener() {
