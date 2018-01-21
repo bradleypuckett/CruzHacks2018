9f0a82c2c3042a4df6f709df39d4134f099be8cf
mocklee <mock7ee@gmail.com>
2018-01-21 11:30:00 -0800

    Fixed profile page

--- Application/app/src/main/java/lango/link/com/langolink/Profile.java
+++ Application/app/src/main/java/lango/link/com/langolink/Profile.java
@@ -3,6 +3,7 @@ package lango.link.com.langolink;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
@@ -11,6 +12,17 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        call = (Button)findViewById(R.id.call_button);
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCall();
+            }
+        });
+    }
+    private void launchCall() {
         Intent intent = new Intent(Profile.this, Chat.class);
         startActivity(intent);
     }
