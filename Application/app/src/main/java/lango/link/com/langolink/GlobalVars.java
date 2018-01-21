53e06ce444350dc79ca08a4d795107d58a9da455
Milkhais Haile <MilkhaisHaile@amanuels-MacBook-Air.local>
2018-01-20 23:53:05 -0800

    stuff

---
+++ Application/app/src/main/java/lango/link/com/langolink/GlobalVars.java
@@ -0,0 +1,17 @@
+package lango.link.com.langolink;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+/**
+ * Created by MilkhaisHaile on 1/20/18.
+ */
+
+public class GlobalVars {
+
+    public static String ip;
+    public static int port;
+    public static PrintWriter out;
+    public static BufferedReader in;
+
+}
--- Application/app/src/main/java/lango/link/com/langolink/Login.java
+++ Application/app/src/main/java/lango/link/com/langolink/Login.java
@@ -1,6 +1,7 @@
 package lango.link.com.langolink;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,31 +10,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Login extends AppCompatActivity {
 
     private EditText user_Name;
     private EditText password;
     private Button login;
     private Button create_account;
+    private String username;
+    private String pass;
+
+
+    Socket kkSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
 
-        // Initialize all the buttons and boxes
-        user_Name = (EditText)findViewById(R.id.username_edit_text);
-        password = (EditText)findViewById(R.id.password_edit_text);
-        login = (Button)findViewById(R.id.login_button);
-        create_account = (Button)findViewById(R.id.create_account_button);
+        try {
+            kkSocket = new Socket(hostName, portNumber);
+            out = new PrintWriter(kkSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+        }catch(Exception e){
+
+        }
 
         // Executes if user clicks login
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(( user_Name.getText().toString().length() != 0) && ( password.getText().toString().length() != 0) ){
-                    // Validate account
+                username = user_Name.getText().toString();
+                pass = password.getText().toString();
+
+                if(( username.length() != 0) && ( pass.length() != 0) ){
+                    // Validate accounts
+                    //if( sendLogin( GlobalVars.out, GlobalVars.in, , )
+
                     launchMain();
                 }
                 else{
@@ -50,6 +69,21 @@ public class Login extends AppCompatActivity {
                 launchCreate();
             }
         });
+    }
+
+    // Initialize all the buttons and boxes
+    private void init() {
+        user_Name = (EditText)findViewById(R.id.username_edit_text);
+        password = (EditText)findViewById(R.id.password_edit_text);
+        login = (Button)findViewById(R.id.login_button);
+        create_account = (Button)findViewById(R.id.create_account_button);
+
+        GlobalVars.ip = "127.0.0.1";
+        GlobalVars.port = 55455;
+        GlobalVars.out = null;
+        GlobalVars.in = null;
+
+
     }
 
     private void launchMain() {
